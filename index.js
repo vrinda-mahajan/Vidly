6de1a56ade191f1649f0d41b7e9c89971c41a0de
vrinda-mahajan <vrindamahajan96@gmail.com>
2023-07-10 13:29:26 +0530

    added register user route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rental");
+const users = require("./routes/users");
 
 const mongoose = require("mongoose");
 mongoose
@@ -19,6 +20,7 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Vidly API services.");
---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { User, validate } = require("../models/userModel");
+const router = express.Router();
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+
+
+router.post("/",async (req,res)=>{
+    const {error} = validate(req.body);
+    if (error) return res.status(404).send(error.details[0].message);
+
+    let user = await User.findOne({email:req.body.email});
+    console.log(user);
+    if(user)  return res.status(400).send("User already registered.");
+
+    user = new User(_.pick(req.body,['name','email','password']))
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+    
+    res.send(_.pick(user,['_id','name','email']));
+})
+
+module.exports = router;
